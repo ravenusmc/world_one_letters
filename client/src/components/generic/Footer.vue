7c43d4507a085f8f4060ae40af1a05a02fc15f4b
ravenusmc <mcuddy77@gmail.com>
2020-03-30 11:58:15 -0400

    added a footer page into the project

---
+++ client/src/components/generic/Footer.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <footer>
+      <div class='footer_fix'>
+        <h2 class='footerTitle font'>Copyright &copy; 2020 Double Sun Productions<br></h2>
+          <div>
+            <a href="https://www.linkedin.com/in/mcuddy1/"><i class="fa fa-linkedin-square fa-3x" aria-hidden="true"></i></a>
+            <a href="https://twitter.com/mcuddy7"><i class="fa fa-twitter-square fa-3x" aria-hidden="true"></i></a>
+            <a href="https://codingsaga.wordpress.com/"><i class="fa fa-pencil fa-3x" aria-hidden="true"></i></a>
+            <a href="https://github.com/ravenusmc"><i class="fa fa-github-square fa-3x" aria-hidden="true"></i></a>
+          </div>
+      </div>
+    </footer>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Footer',
+};
+</script>
+
+<style scoped>
+.fa-linkedin-square, .fa-twitter-square, .fa-pencil, .fa-github-square {
+  color: white;
+  margin: 5px;
+}
+
+footer {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background: #333;
+  height: 300px;
+}
+
+.footer_fix {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  margin-bottom: 50px;
+  padding-top: 50px;
+}
+
+.footerTitle {
+  font-size: 20px;
+  text-transform: uppercase;
+  color: white;
+}
+</style>
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,17 +1,20 @@
 <template>
   <div>
     <Header/>
+    <Footer/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import Header from '@/components/generic/Header.vue'
+import Footer from '@/components/generic/Footer.vue'
 
 export default {
   name: 'Home',
   components: {
     Header,
+    Footer,
   }
 }
 </script>
