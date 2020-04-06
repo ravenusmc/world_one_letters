5a0d0d2a7097b2baca06667e30cbd488f6b828b5
ravenusmc <mcuddy77@gmail.com>
2020-04-06 16:03:33 -0400

    working on getting data for analysis

--- server/data.py
+++ server/data.py
@@ -1,6 +1,7 @@
 #importing supporting libraries
 import numpy as np
 import pandas as pd
+import json
 import datetime
 from textblob import TextBlob
 
@@ -10,19 +11,38 @@ class Data():
 
     def __init__(self):
         self.index = pd.read_csv('./data/index.csv')
-        self.letters = pd.read_json('./data/letters.json')
+        #self.letters = pd.read_json('./data/letters.json')
+        self.letters = pd.read_json('./data/letters.json', typ='series')
         #self.data['deadline'] = pd.to_datetime(self.data['deadline'], infer_datetime_format=True)
 
+    def data_idea_test(self):
+        # Works
+        #data = json.load(open('./data/letters.json'))
+        # print(type(data))
+        # End works!!!
 
-    # My plan for the data analysis - I need to get the data broken out by two dates
-    # That the user enters.
-    # I need to ensure that the letters are only in English - this needs to be
-    # the first sort.
+        #This will get the text in the series
+        print(self.letters.loc["na_uk_16"])
+
     # Place is tied to to the places.csv that also has country in it
     # I may have to break each letter up by using the tokenization feature on
     #Text blob
     def get_sentiment_for_letters(self):
-        pass
+        # Getting the letters in the data set that are only in Engish
+        data_set_english_only = self.index[(self.index.language == 'english')]
+        years = [1914, 1915, 1916, 1917]
+        months = [1,2,3,4,5,6,7,8,9,10,11,12]
+        for year in years:
+            #resetting the data set for each loop
+            data_set_english_only_loop = data_set_english_only
+            year_data_set = data_set_english_only_loop[(data_set_english_only_loop.year == year)]
+            for month in months:
+                #resetting the dataset for each loop
+                year_data_set_loop = year_data_set
+                month_data_set = year_data_set_loop[(year_data_set_loop.month == month)]
+                print(month)
+        # print(data_set_english_only['year'].min()) # 1914.0
+        # print(data_set_english_only['year'].max()) # 1917.0
 
     #This method will get the sentiment of the lines spoken by the four main characters
     #for each season. How this will work is that each line, by character, will get its sentiment
@@ -53,3 +73,6 @@ class Data():
             rows.append(float(average))
             season_data.append(rows)
         return season_data
+
+data = Data()
+data.get_sentiment_for_letters()
